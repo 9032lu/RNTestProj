b5d6de577a6a2a021e941d29155f67bd7de3786a
9032lu <511815816@qq.com>
2019-04-23 11:24:27 +0800

    添加路由测试

---
+++ AppTest2.js
@@ -0,0 +1,113 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View,PixelRatio,Button} from 'react-native';
+import {createStackNavigator,createAppContainer} from 'react-navigation'
+
+ class HomeScreen extends Component {
+  render() {
+    return (
+        <View style={styles.flex}>
+          <Text style={{fontSize:20}}>首页</Text>
+          <Button 
+            title='Goto Detail'
+            onPress={()=>this.props.navigation.navigate('Details')}
+          />
+
+
+        </View>
+      
+    );
+  }
+}
+
+class DetailsScreen extends Component{
+  render() {
+    return (
+      <View style={styles.flex}>
+        <Text style={{fontSize:20}}>详情</Text>
+      </View>
+    )
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+  Home:HomeScreen,
+  Details:DetailsScreen,
+
+  },
+  {
+    initialRouteName:'Home',
+  }
+);
+const Appcontainer =createAppContainer(RootStack);
+
+export default class AppTest2 extends Component {
+  render() {
+    return (
+      <Appcontainer/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  flex:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'rgb(234,234,234)',
+  },
+  container:{
+    marginTop:0,
+    marginLeft:5,
+    marginRight:5,
+    height:84,
+    flexDirection:'row',
+    borderRadius:5,
+    padding:2,
+    backgroundColor:'#ff0067',
+  },
+  item:{
+    flex:1,
+    height:80,
+  },
+  font:{
+    color:'white',
+    fontSize:16,
+    fontWeight:'bold',
+  },
+  center:{
+    justifyContent:'center',
+    alignItems:'center',
+
+  },
+  color1:{
+    // backgroundColor:'black',
+
+  },
+  color2:{
+    // backgroundColor:'blue',
+
+  },
+  color3:{
+    // backgroundColor:'yellow',
+
+  },
+  lineLeftRigth:{
+    borderLeftWidth:1,
+    borderRightWidth:1,
+    borderColor:'#fff',
+
+  },
+  lineCenter:{
+    borderBottomWidth:1,
+    borderColor:'white',
+  }
+});
--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import {AppRegistry} from 'react-native';
 // import App from './App';
 import {name as appName} from './app.json';
 import AppTest1 from './AppTest1';
+import AppTest2 from './AppTest2'
 
-AppRegistry.registerComponent(appName, () => AppTest1);
+AppRegistry.registerComponent(appName, () => AppTest2);
